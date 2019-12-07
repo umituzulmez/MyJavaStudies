85bed8e1cacd92238e1fe9628bfa2d4d1246b9ad
Umit Uzulmez <umituzulmez@gmail.com>
2019-12-07 21:04:01 +0000

    change

--- src/day_39_finalKeyword/TestMouse.java
+++ src/day_39_finalKeyword/TestMouse.java
@@ -5,7 +5,7 @@ public class TestMouse {
 	public static void main(String[] args) {
 
 		Mouse mouse = new Mouse();
-		//mouse.getRodenDetails();
+		mouse.getRodenDetails();
 		mouse.getMouseDetails();
 
 	}
