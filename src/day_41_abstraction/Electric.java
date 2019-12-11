a52e1edab050c5fcdcd9918fa6bc00cf89e271f2
Umit Uzulmez <umituzulmez@gmail.com>
2019-12-11 21:51:40 +0000

    day41 abstraction

--- src/day_41_abstraction/Electric.java
+++ src/day_41_abstraction/Electric.java
@@ -2,7 +2,22 @@ package day_41_abstraction;
 
 public interface Electric {
 	
+	public static final boolean HAS_BATTERIES = true;
+	
+	boolean HAS_BATTERIES2 = false; //public static final
+	
 	public abstract void charge();
 	
+	void charge2(); //public abstract
+	
+	public default void methodA() {
+		
+		//code
+	}
+	
+	public static void methodB() {
+		
+		//code
+	}
 
 }
--- src/day_41_abstraction/SelfDriving.java
+++ src/day_41_abstraction/SelfDriving.java
@@ -2,4 +2,5 @@ package day_41_abstraction;
 
 public interface SelfDriving {
 
+	public abstract void selfDrive();
 }
