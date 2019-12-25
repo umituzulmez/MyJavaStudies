44e564cf558ebe17f2a23343495501560947567c
Umit Uzulmez <umituzulmez@gmail.com>
2019-12-25 16:59:32 +0000

    some changes

--- src/day26_arrays_part3/MethodArrays2.java
+++ src/day26_arrays_part3/MethodArrays2.java
@@ -8,7 +8,6 @@ public class MethodArrays2 {
 	public static void main(String[] args) {
 		
 		System.out.println(findCar(3,"Honda"));
-		
 
 	}
 	
--- src/freeRecap/taskkkkk.java
+++ src/freeRecap/taskkkkk.java
@@ -6,48 +6,23 @@ import java.util.Scanner;
 public class taskkkkk {
 
 	public static void main(String[] args) {
-	
-	
-	String info1 = "This info is used for creation password : xxxCD132Gxxx . Please do not share with anyone";
-		
-	String info2 = "This info is used for creation password : xxx00ABC!xxx . Please do not share with anyone";
-	
-	
-		String[] bölünmüş1 = info1.split("xxx "); 
-		
-		System.out.println(Arrays.toString(bölünmüş1));
-		
-		System.out.println(bölünmüş1[8]);
-		
-		String[] bölünmüş2 = info2.split(" "); 
-		
-		System.out.println(Arrays.toString(bölünmüş2));
-		
-		System.out.println(bölünmüş2[8]);
-		
-		String[] şifre1 = bölünmüş1[8].split("xxx");
-		
-		System.out.println(Arrays.toString(şifre1));
-		
-		System.out.println(şifre1.length);
-		
-		System.out.println(şifre1[1]);
-		
-		String[] şifre2 = bölünmüş2[8].split("xxx");
-		
-		System.out.println(Arrays.toString(şifre2));
-		
-		System.out.println(şifre2.length);
-		
-		System.out.println(şifre2[1]);
-		
-		String sonŞifre = şifre1[1] + şifre2[1];
-		
-		System.out.println("Şifre : " + sonŞifre);
-		
-		System.out.println(sonŞifre);
-		
-		
+	    Scanner scan = new Scanner(System.in);
+	    int num = scan.nextInt();
+	    isPalindrome(num);
+	  }
+	  
+	  public static void isPalindrome(int num){
+	   
+	   int num2 = num;
+	   String reversedNum = "";
+	   while(num2>0){
+	     
+	     reversedNum += num2%10;
+	     num2 = num2/10;
+	   }
+	    
+	    int newNum = Integer.parseInt(reversedNum);
+	    
+	    System.out.println(num==newNum);
+	  }
 	}
-		
-}
